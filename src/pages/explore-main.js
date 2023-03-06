b4b1512b358c8fa8714540584dafb9128842ec70
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-03-06 12:43:14 +0000

    add pages
    add hompage, sign-in/sign-up, landing, explore-main, explore-user-profile

---
+++ src/pages/explore-main.js
@@ -0,0 +1,5 @@
+import styles from '@/styles/ExploreMain.module.css';
+
+export default function ExploreMain() {
+  return <h1 className={styles.test}>Explore main page</h1>;
+}
---
+++ src/pages/explore-reflect.js
@@ -0,0 +1,5 @@
+import styles from '@/styles/ExploreReflect.module.css';
+
+export default function ExploreReflect() {
+  return <h1 className={styles.test}>Explore & Reflect landing page</h1>;
+}
