b269feea37005474670f9a7fd079aae605fcaa57
LauraK0 <laurakeating@live.co.uk>
2023-03-06 17:48:51 +0000

    layout component added to art card page

--- src/components/ArtCard.js
+++ src/components/ArtCard.js
@@ -1,11 +1,14 @@
 import StarRating from './StarRating';
 import style from '../styles/ArtCard.module.css';
+import Layout from './Layout';
 
 export default function ArtCard() {
   return (
-    <div className={style.cardContainer}>
-      <h3>Art Card</h3>
-      <StarRating value={3.4} />
-    </div>
+    <Layout>
+      <div className={style.cardContainer}>
+        <h3>Art Card</h3>
+        <StarRating value={3.4} />
+      </div>
+    </Layout>
   );
 }
