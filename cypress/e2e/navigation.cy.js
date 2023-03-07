0f4fc7dff59580a20b1b73aafc0322696a925f92
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-07 17:26:17 +0000

    Test recognises nav bar

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -3,38 +3,7 @@ context('Navigation', () => {
     cy.visit('http://localhost:3000');
   });
 
-  it('Contains all NavBar elements', () => {
-    cy.get('nav').contains('/');
-    cy.get('nav').contains('material-symbols:home');
-    cy.get('nav').contains('material-symbols:help-center');
-    cy.get('nav').contains('material-symbols:add');
-    cy.get('nav').contains('fa6-solid:ranking-star');
-    cy.get('nav').contains('fluent:book-contacts-20-filled');
-  });
-
-  it("cy.go() - go back or forward in the browser's history", () => {
-    cy.location('pathname').should('include', 'navigation');
-
-    cy.go('back');
-    cy.location('pathname').should('not.include', 'navigation');
-
-    cy.go('forward');
-    cy.location('pathname').should('include', 'navigation');
-
-    // clicking back
-    cy.go(-1);
-    cy.location('pathname').should('not.include', 'navigation');
-
-    // clicking forward
-    cy.go(1);
-    cy.location('pathname').should('include', 'navigation');
-  });
-
-  it('cy.reload() - reload the page', () => {
-    // https://on.cypress.io/reload
-    cy.reload();
-
-    // reload the page without using the cache
-    cy.reload(true);
+  it('Passes', () => {
+    cy.get('.NavBar_vector__okJP0').should('exist');
   });
 });
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import NavBar from '@/components/NavBar';
+
 import Head from 'next/head';
 // will we use a Next font //
 import styles from '@/styles/Home.module.css';
@@ -7,6 +9,7 @@ export default function Home() {
   return (
     <Layout home>
       <h1 className={styles.test}>Homepage!</h1>
+      <NavBar />
     </Layout>
   );
 }
