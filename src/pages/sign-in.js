cb49241d475e561036b79c233faa1f6b683fac0b
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-03-07 17:38:30 +0000

    add supabase tutorial files

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,5 +1,22 @@
 import styles from '@/styles/SignIn.module.css';
+import { Auth } from '@supabase/auth-ui-react';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import Account from '../components/Account';
 
 export default function SignIn() {
-  return <h1 className={styles.test}>Sign Up / Sign In</h1>;
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  return (
+    <>
+      <h1 className={styles.test}>Sign Up / Sign In</h1>
+      <div className="container">
+        {!session ? (
+          <Auth supabaseClient={supabase} />
+        ) : (
+          <Account session={session} />
+        )}
+      </div>
+    </>
+  );
 }
