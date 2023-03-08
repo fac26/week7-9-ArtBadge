a2e4689baf87d98dc186c9fb482727fa65d4461b
LauraK0 <laurakeating@live.co.uk>
2023-03-08 14:39:27 +0000

    missing semi colons added

--- src/pages/reflect-personal-badges.js
+++ src/pages/reflect-personal-badges.js
@@ -1,10 +1,10 @@
 import styles from '@/styles/ReflectPersonalBadges.module.css';
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 export default function ReflectPersonalBadges() {
   return (
   <Layout>
-  <h1 className={styles.test}>Reflect personal badges</h1>
+    <h1 className={styles.test}>Reflect personal badges</h1>
   </Layout>
-
+  );
 }
