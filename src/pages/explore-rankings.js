96a69935467b54c4c6fdb137fcf85b30c84c67c2
LauraK0 <laurakeating@live.co.uk>
2023-03-08 15:03:59 +0000

    rankings readded

---
+++ src/pages/explore-rankings.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function ExploreRankings() {
+  return (
+    <Layout>
+      <h1 className={styles.test}>Rankings</h1>
+    </Layout>
+  );
+}
--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -2,7 +2,6 @@ import styles from '@/styles/SignIn.module.css';
 import { Auth } from '@supabase/auth-ui-react';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import Account from '../components/Account';
-// import Router from 'next/router'
 
 export default function SignIn() {
   const session = useSession();
@@ -15,12 +14,6 @@ export default function SignIn() {
         {!session ? (
           <Auth supabaseClient={supabase} />
         ) : (
-        //   Router.push({
-        //     pathname:'/explore-main',
-        //     query: { example: 'something' },                // query is optional
-        // }).then(() => {
-        //     window.scrollTo(0, 0)
-        // })
           <Account session={session} />
         )}
       </div>
