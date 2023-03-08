cacfd236d65d8c139583c7394f92e90b1a3a964f
LauraK0 <laurakeating@live.co.uk>
2023-03-08 12:13:43 +0000

    sign-in form styles added to globals css

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -76,7 +76,7 @@ export default function Avatar({ uid, url, size, onUpload }) {
         </label>
         <input
           style={{
-            visibility: 'visible',
+            visibility: 'hidden',
             position: 'absolute',
           }}
           type="file"
--- src/pages/reflect-personal-profile.js
+++ src/pages/reflect-personal-profile.js
@@ -1,5 +1,16 @@
 import styles from '@/styles/ReflectPersonalProfile.module.css';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import Account from '../components/Account';
 
 export default function ReflectPersonalProfile() {
-  return <h1 className={styles.test}>Reflect personal profile</h1>;
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  return (
+    <>
+      <h1 className={styles.test}>Reflect personal profile</h1>
+      <div>{JSON.stringify(session)}</div>
+      <div>{session.user.email}</div>
+    </>
+  );
 }
