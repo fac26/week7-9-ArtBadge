d0908318083a85a2ec3624a67f38246c62d0dffd
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-09 12:22:32 +0000

    Git clash resolution

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,7 +1,9 @@
 context('Navigation', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000/explore-reflect');
-  });
+  cy.visit('http://localhost:3000');
+  cy.visit('http://localhost:3000/explore-reflect');
+  cy.visit('http://localhost:3000/reflect-add-image');
+  cy.visit('http://localhost:3000/explore-rankings');
+  cy.visit('http://localhost:3000//reflect-personal-contact');
 
   it('Passes', () => {
     cy.get('[data-cy="home"]').should('exist');
