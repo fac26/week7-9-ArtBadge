ee2f3d13960717dbdfd1e9b1efadcb8df58fcb34
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-10 14:45:10 +0000

    Git conflict

--- src/pages/explore-main.js
+++ src/pages/explore-main.js
@@ -5,17 +5,16 @@ import Layout from '../components/Layout';
 import supabase from '../../api.js';
 import { useRouter } from 'next/router';
 
-export async function getServerProps() {
-  const { data, error } = await supabase
-    .from('profiles')
-    .select()
-    .eq('public', 'true');
+export async function getServerSideProps() {
+  // console.log(supabase.auth.user());
+  const { data, error } = await supabase.from('profiles').select();
   return { props: { post: data } };
 }
 
 export default function ExploreMain(props) {
   const router = useRouter();
-  console.log(props);
+  // console.log(JSON.stringify(props));
+  console.log(props.post);
   return (
     <Layout>
       <h1 className={styles.test}>Explore main page</h1>
