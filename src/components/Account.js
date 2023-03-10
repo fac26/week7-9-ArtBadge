a642b1c525059bcea34322afb71b60e130b20093
Niete <nieteis@gmail.com>
2023-03-10 11:45:44 +0000

    Redirects to landing page after updating profile

--- src/components/Account.js
+++ src/components/Account.js
@@ -55,7 +55,8 @@ export default function Account({ session }) {
 
       let { error } = await supabase.from('profiles').upsert(updates);
       if (error) throw error;
-      alert('Profile updated!');
+      alert('Profile has updated!');
+      window.location.href = '/explore-reflect';
     } catch (error) {
       alert('Error updating the data!');
       console.log(error);
