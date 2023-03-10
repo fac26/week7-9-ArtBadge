76a4d23af33c242f06a8bce456ad6c6c4041e915
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-10 12:12:13 +0000

    API Key testing

--- src/pages/explore-main.js
+++ src/pages/explore-main.js
@@ -2,8 +2,20 @@ import styles from '@/styles/ExploreMain.module.css';
 import NavBar from '../components/NavBar';
 import ArtCard from '@/components/ArtCard';
 import Layout from '../components/Layout';
+import { supabase } from '../../api.js';
+import { useRouter } from 'next/router';
 
-export default function ExploreMain() {
+export async function getServerProps() {
+  const { data, error } = await supabase
+    .from('profiles')
+    .select()
+    .eq('public', 'true');
+  return { props: { post: data } };
+}
+
+export default function ExploreMain(props) {
+  const router = useRouter();
+  console.log(props);
   return (
     <Layout>
       <h1 className={styles.test}>Explore main page</h1>
