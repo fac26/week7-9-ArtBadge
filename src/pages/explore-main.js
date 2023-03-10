acb26860001e022e2edb05342f00c094f2543757
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-10 12:15:50 +0000

    API Key

--- src/pages/explore-main.js
+++ src/pages/explore-main.js
@@ -2,7 +2,7 @@ import styles from '@/styles/ExploreMain.module.css';
 import NavBar from '../components/NavBar';
 import ArtCard from '@/components/ArtCard';
 import Layout from '../components/Layout';
-import { supabase } from '../../api.js';
+import supabase from '../../api.js';
 import { useRouter } from 'next/router';
 
 export async function getServerProps() {
