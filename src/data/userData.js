01fd1a70bfdc2b28939c2a01724b9dceaf435ea5
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-10 16:46:49 +0000

    Dynamic props paths

--- src/data/userData.js
+++ src/data/userData.js
@@ -1,46 +1,46 @@
-const userData = [
-  {
-    id: 1,
-    name: 'Eva',
-    userName: 'ILoveArt4Eva',
-    image: '/avatar8.png',
-    badgeLevel: '6',
-  },
-  {
-    id: 2,
-    name: 'Dominic',
-    userName: 'ArtLover',
-    image: '/avatar7.png',
-    badgeLevel: '4',
-  },
-  {
-    id: 3,
-    name: 'Gal',
-    userName: 'ArtWolf',
-    image: '/avatar1.png',
-    badgeLevel: '10',
-  },
-  {
-    id: 4,
-    name: 'Laura',
-    userName: 'ArtCollector',
-    image: '/avatar3.png',
-    badgeLevel: '9',
-  },
-  {
-    id: 5,
-    name: 'Niete',
-    userName: 'ArtBear',
-    image: '/avatar5.png',
-    badgeLevel: '2',
-  },
-  {
-    id: 6,
-    name: 'Harriet',
-    userName: 'ArtBadger',
-    image: '/avatar2.png',
-    badgeLevel: '1',
-  },
-];
+// const userData = [
+//   {
+//     id: 1,
+//     name: 'Eva',
+//     userName: 'ILoveArt4Eva',
+//     image: '/avatar8.png',
+//     badgeLevel: '6',
+//   },
+//   {
+//     id: 2,
+//     name: 'Dominic',
+//     userName: 'ArtLover',
+//     image: '/avatar7.png',
+//     badgeLevel: '4',
+//   },
+//   {
+//     id: 3,
+//     name: 'Gal',
+//     userName: 'ArtWolf',
+//     image: '/avatar1.png',
+//     badgeLevel: '10',
+//   },
+//   {
+//     id: 4,
+//     name: 'Laura',
+//     userName: 'ArtCollector',
+//     image: '/avatar3.png',
+//     badgeLevel: '9',
+//   },
+//   {
+//     id: 5,
+//     name: 'Niete',
+//     userName: 'ArtBear',
+//     image: '/avatar5.png',
+//     badgeLevel: '2',
+//   },
+//   {
+//     id: 6,
+//     name: 'Harriet',
+//     userName: 'ArtBadger',
+//     image: '/avatar2.png',
+//     badgeLevel: '1',
+//   },
+// ];
 
-export default userData;
+// export default userData;
--- src/pages/user-profile/[id].js
+++ src/pages/user-profile/[id].js
@@ -5,11 +5,17 @@ import supabase from '../../../api.js';
 import { useRouter } from 'next/router';
 
 export async function getStaticProps({ params }) {
-  const { data, error } = await supabase
+  const { data, error_user } = await supabase
     .from('profiles')
     .select()
     .eq('username', params.id);
-  return { props: { data } };
+  const { posts, error_posts } = await supabase
+    .from('artData')
+    .select()
+    .eq('user_uuid', data.id);
+  console.log(posts);
+  console.log(data.id);
+  return { props: { data: data[0] } };
 }
 
 export async function getStaticPaths() {
@@ -24,10 +30,11 @@ export async function getStaticPaths() {
 export default function UserProfile({ data }) {
   const router = useRouter();
   // console.log(JSON.stringify(props));
-  console.log({ data });
+  // console.log({ data });
+  // console.log({ posts });
   return (
     <Layout>
-      <h1 className={styles.test}>{data[0].username}</h1>
+      <h1 className={styles.test}>{data.username}</h1>
       <ArtCard />
     </Layout>
   );
