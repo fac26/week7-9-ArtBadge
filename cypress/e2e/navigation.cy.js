3957b9cc20ff40a12011b5772cb2ec76c601e4ad
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-11 20:56:13 +0000

    Cypress testing

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,26 +1,15 @@
-describe('Navigation Bar', () => {
-  it('should have all required navigation bar elements', () => {
-    cy.visit('http://www.localhost:3000/explore-reflect'); // navigate to the home/explore-reflect page containing the navigation bar
-    cy.get('[data-cy="home"]').should('exist');
+context('Navigation', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
   });
 
-  it('should have all required navigation bar elements', () => {
-    cy.visit('http://www.localhost:3000'); // navigate to the help page containing the navigation bar
-    cy.get('[data-cy="help"]').should('exist');
-  });
-
-  it('should have all required navigation bar elements', () => {
-    cy.visit('http://www.localhost:3000/reflect-add-image'); // navigate to the add page containing the navigation bar
-    cy.get('[data-cy="add"]').should('exist');
-  });
-
-  it('should have all required navigation bar elements', () => {
-    cy.visit('http://www.localhost:3000/explore-rankings'); // navigate to the ranking star page containing the navigation bar
-    cy.get('[data-cy="ranking-star"]').should('exist');
-  });
-
-  it('should have all required navigation bar elements', () => {
-    cy.visit('http://www.localhost:3000/reflect-personal-contact'); // navigate to the book contacts page containing the navigation bar
-    cy.get('[data-cy="book-contacts"]').should('exist');
+  it('Passes', () => {
+    cy.get('.Layout').within(() => {
+      cy.get('[data-cy="home"]').should('exist');
+      cy.get('[data-cy="help"]').should('exist');
+      cy.get('[data-cy="add"]').should('exist');
+      cy.get('[data-cy="ranking-star"]').should('exist');
+      cy.get('[data-cy="book-contacts"]').should('exist');
+    });
   });
 });
