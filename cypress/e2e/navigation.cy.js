3840b1e126f3ba5c157a8639ebe0cfcdae7ba915
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-11 21:22:47 +0000

    Up to date Cypress Testing of nav bar passes

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -8,4 +8,14 @@ describe('NavBar component', () => {
     cy.visit('http://localhost:3000/reflect-add-image'); // Replace with the actual URL you want to test
     cy.get('[data-cy="add"]').should('exist');
   });
+
+  it('should exist on the page', () => {
+    cy.visit('http://localhost:3000/explore-rankings'); // Replace with the actual URL you want to test
+    cy.get('[data-cy="ranking-star"]').should('exist');
+  });
+
+  it('should exist on the page', () => {
+    cy.visit('http://localhost:3000/reflect-personal-contact'); // Replace with the actual URL you want to test
+    cy.get('[data-cy="book-contacts"]').should('exist');
+  });
 });
