5b3532fb6e2c286988508a55256ba693de1f478e
LauraK0 <laurakeating@live.co.uk>
2023-03-11 10:27:35 +0000

    art posts rendered on user page, some styling updated

--- src/components/ArtCard.js
+++ src/components/ArtCard.js
@@ -1,13 +1,28 @@
 import StarRating from './StarRating';
-import style from '../styles/ArtCard.module.css';
+import styles from '../styles/ArtCard.module.css';
 import Layout from './Layout';
+import Image from 'next/image';
+import { Icon } from '@iconify/react';
 
-export default function ArtCard() {
+export default function ArtCard({ post }) {
+  console.log(post);
+  const { title, image, location, rating } = post;
   return (
     <Layout>
-      <div className={style.cardContainer}>
-        <h3>Art Card</h3>
-        <StarRating value={3.4} />
+      <div className={styles.cardContainer}>
+        <Image
+          src={image}
+          className={styles.image}
+          width={300}
+          height={300}
+          alt={title}
+        />
+        <h3>{title}</h3>
+        <StarRating value={rating} />
+        <div className={styles.locationDiv}>
+          <Icon className={styles.vector} icon="material-symbols:location-on" />
+          <p>{location}</p>
+        </div>
       </div>
     </Layout>
   );
