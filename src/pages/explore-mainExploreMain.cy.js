0f11fac957e86626b5b54d49489c26d3a3410a55
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-12 16:54:25 +0000

    Deleted explore-mainExplore-Main file

--- src/pages/explore-mainExploreMain.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ExploreMain from './explore-main'
-
-describe('<ExploreMain />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ExploreMain />)
-  })
-})
