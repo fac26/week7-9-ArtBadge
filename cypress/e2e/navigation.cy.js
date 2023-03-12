c8f039f785d37f31a0b88fa631a8206c498a1477
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-12 17:08:39 +0000

    Final NavBar testing push

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,21 +1,21 @@
 describe('NavBar component', () => {
   it('should exist on the page', () => {
-    cy.visit('http://localhost:3000/explore-main'); // Replace with the actual URL you want to test
+    cy.visit('http://localhost:3000/explore-main');
     cy.get('[data-cy="home"]').should('exist');
   });
 
   it('should exist on the page', () => {
-    cy.visit('http://localhost:3000/reflect-add-image'); // Replace with the actual URL you want to test
+    cy.visit('http://localhost:3000/reflect-add-image');
     cy.get('[data-cy="add"]').should('exist');
   });
 
   it('should exist on the page', () => {
-    cy.visit('http://localhost:3000/explore-rankings'); // Replace with the actual URL you want to test
+    cy.visit('http://localhost:3000/explore-rankings');
     cy.get('[data-cy="ranking-star"]').should('exist');
   });
 
   it('should exist on the page', () => {
-    cy.visit('http://localhost:3000/reflect-personal-contact'); // Replace with the actual URL you want to test
+    cy.visit('http://localhost:3000/reflect-personal-contact');
     cy.get('[data-cy="book-contacts"]').should('exist');
   });
 });
