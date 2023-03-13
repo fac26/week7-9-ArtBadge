9df2b1e9463213379cb5b33d9e41201a2a8464fe
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-03-13 12:02:55 +0000

    userbar component initial commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,22 @@
 import styles from '../styles/Layout.module.css';
 import NavBar from '@/components/NavBar';
+import Userbar from './Userbar';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 export default function Layout({ children, home }) {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+  console.log('session', session);
+  console.log('supabase', supabase);
+  // const session = true;
   return (
-    <div className={styles.test}>
-      <main>{children}</main>
-      {!home ? <NavBar /> : null}
-    </div>
+    <>
+      <header className="userbar">{session ? <Userbar /> : null}</header>
+
+      <div className={styles.test}>
+        <main>{children}</main>
+        {!home ? <NavBar /> : null}
+      </div>
+    </>
   );
 }
---
+++ src/components/Userbar.js
@@ -0,0 +1,5 @@
+const Userbar = () => {
+  return <h1>this is the h1</h1>;
+};
+
+export default Userbar;
