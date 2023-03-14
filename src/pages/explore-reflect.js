a9dba3915f4d40c68c50a84861050f07d4925f3c
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-03-14 10:48:58 +0000

    landing page content

--- src/pages/explore-reflect.js
+++ src/pages/explore-reflect.js
@@ -5,10 +5,23 @@ import Link from 'next/link';
 export default function ExploreReflect() {
   return (
     <Layout>
-      <h1 className={styles.test}>Explore & Reflect landing page</h1>
-      <Link href="/explore-main">Explore</Link>
+      <h1 className={styles.header}>ArtBadge</h1>
+      <h2 className={styles.tag}>
+        Upload and share your favourite artwork with your friends
+      </h2>
+      <button>
+        <Link href="/explore-main">
+          EXPLORE<br></br>
+          new art that others have experienced
+        </Link>
+      </button>
       <br></br>
-      <Link href="/reflect-personal-profile">Reflect</Link>
+      <button>
+        <Link href="/reflect-personal-profile">
+          REFLECT<br></br>
+          on art that you have seen
+        </Link>
+      </button>
     </Layout>
   );
 }
