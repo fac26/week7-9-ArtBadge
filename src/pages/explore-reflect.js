696454d9894ed0ea657577933379fde704998153
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-15 12:59:53 +0000

    CSS styling on explore-reflect page

--- src/pages/explore-reflect.js
+++ src/pages/explore-reflect.js
@@ -7,19 +7,19 @@ export default function ExploreReflect() {
     <Layout>
       <div className={styles.container}>
         <div className={styles.overlay}>
-          <h1 className={styles.header}>ArtBadge</h1>
+          <h1 className={styles.header}>ART BADGE</h1>
           <h2 className={styles.tag}>
             Upload and share your favourite artwork with your friends
           </h2>
           <Link href="/explore-main">
-            <button>
+            <button className={styles.explorebutton}>
               EXPLORE<br></br>
               new art that others have experienced
             </button>
           </Link>
           <br></br>
           <Link href="/reflect-personal-profile">
-            <button>
+            <button className={styles.reflectbutton}>
               REFLECT<br></br>
               on art that you have seen
             </button>
