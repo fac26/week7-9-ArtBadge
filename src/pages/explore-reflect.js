1d36b58cb450c0877c87b82775d87ccb998b1b6d
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-15 14:54:13 +0000

    Explore reflect page CSS styling

--- src/pages/explore-reflect.js
+++ src/pages/explore-reflect.js
@@ -51,19 +51,21 @@ export default function ExploreReflect() {
               <h2 className={styles.tag}>
                 Upload and share your favourite artwork with your friends
               </h2>
-              <Link href="/explore-main">
-                <button className={styles.explorebutton}>
-                  EXPLORE<br></br>
-                  new art that others have experienced
-                </button>
-              </Link>
-              <br></br>
-              <Link href={`/user-profile/${user.id}`}>
-                <button className={styles.reflectbutton}>
-                  REFLECT<br></br>
-                  on art that you have seen
-                </button>
-              </Link>
+              <div className={styles.displaycircles}>
+                <Link href="/explore-main">
+                  <button className={styles.explorebutton}>
+                    EXPLORE<br></br>
+                    new art that others have experienced
+                  </button>
+                </Link>
+                <br></br>
+                <Link href={`/user-profile/${user.id}`}>
+                  <button className={styles.reflectbutton}>
+                    REFLECT<br></br>
+                    on art that you have seen
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </>
