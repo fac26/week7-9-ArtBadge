9c1f4fcd113b8d00e4d418878989d0fd3c93b950
LauraK0 <laurakeating@live.co.uk>
2023-03-16 14:01:58 +0000

    render badge?

--- src/components/ArtBadge.js
+++ src/components/ArtBadge.js
@@ -1,14 +1,38 @@
 import styles from '../styles/ArtBadge.module.css';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 export default function ArtBadge({ badge }) {
   const { id, title, description, badge_url, badge_silhouette_url } = badge;
-  console.log('badge', badge_silhouette_url);
+  const supabase = useSupabaseClient();
+  const [badgeUrl, setBadgeUrl] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    if (badge_url) downloadImage(badge_url);
+  }, [badge_url]);
+
+  async function downloadImage(badge_url) {
+    try {
+      const { data, error } = await supabase.storage
+        .from('avatars')
+        .download(badge_url);
+      if (error) {
+        throw error;
+      }
+      const url = URL.createObjectURL(data);
+      setBadgeUrl(url);
+    } catch (error) {
+      console.log('Error downloading image: ', error);
+    }
+  }
+
   return (
     <div className={styles.cardContainer}>
       <div className={styles.imageDiv}>
         <Image
-          src={badge_url}
+          src={badgeUrl}
           className={styles.image}
           width={200}
           height={200}
@@ -17,7 +41,10 @@ export default function ArtBadge({ badge }) {
       </div>
       <div className={styles.description}>
         <h3>{title}</h3>
-        <p>{description}</p>
+        <p>
+          {description}
+          {badgeUrl}
+        </p>
       </div>
     </div>
   );
